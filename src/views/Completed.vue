cc8a767364b0460af6d6bcc5a2dd823a20ff321d
Eka Juliantara <eka_juliantara@Ekas-MacBook-Pro.local>
2025-12-02 10:08:59 +0700

    Revert to Routing

--- src/views/Completed.vue
+++ src/views/Completed.vue
@@ -1,30 +1,3 @@
 <template>
-    <h3>Completed ✅</h3>
-    <div>
-      <ul>
-        <li v-for="completedTodo in completedTodos" :key="completedTodo.id" style="margin: 8px 0">
-          {{ completedTodo.text }}
-        </li>
-      </ul>
-    </div>
+  <h3>Completed ✅</h3>
 </template>
-
-<script>
-import { mapState } from 'pinia';
-import { useTodos } from '@/stores/todos';
-
-import HeaderBar from '@/components/Header.vue';
-import FooterBar from '@/components/Footer.vue';
-
-export default {
-  components: {
-    HeaderBar,
-    FooterBar,
-  },
-  computed: {
-    ...mapState(useTodos, [
-      'completedTodos'
-    ])
-  }
-}
-</script>
